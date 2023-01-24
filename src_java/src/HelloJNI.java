e81e3b009021952d384d6ec77ca4a060185d49a5
Banee Ishaque K <Baneeishaque@gmail.com>
2023-01-24 23:24:16 +0530

    path fix

--- src_java/src/HelloJNI.java
+++ src_java/src/HelloJNI.java
@@ -1,7 +1,7 @@
 public class HelloJNI {
     static {
         // Load native library hello.dll (Windows) or libhello.so (Unixes) at runtime. This library contains a native method called sayHello()
-        System.loadLibrary("../src_cpp_lib/hello");
+        System.loadLibrary("../../src_cpp_lib/hello");
     }
 
     // Declare an instance native method sayHello() which receives no parameter and returns void
